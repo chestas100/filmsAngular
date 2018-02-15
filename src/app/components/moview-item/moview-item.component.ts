3af821301a52653eeafdd45ba5f37f19d2d5d8e2
chestas100 <chestas100@gmail.com>
2018-02-15 19:31:47 +0200

    add formGroup

--- src/app/components/moview-item/moview-item.component.ts
+++ src/app/components/moview-item/moview-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-moview-item',
@@ -9,13 +10,15 @@ import { FormControl } from '@angular/forms';
 })
 export class MoviewItemComponent implements OnInit {
   @Input() movie;
-  newInput = new FormControl;
+  movieItemForm = new FormGroup({
+    newInput: new FormControl(),
+  });
 
   ngOnInit() {
     setTimeout(() => {
-      this.newInput.setValue('new value here');
+      this.movieItemForm.setValue({newInput: 'new value here'});
     }, 2000);
-    this.newInput.valueChanges.subscribe(data => {
+    this.movieItemForm.valueChanges.subscribe(data => {
       console.log(data);
     });
   }
